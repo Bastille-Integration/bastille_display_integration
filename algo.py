38ce06f2b08d70b9da37fa3c36561a359c77cf07
dalybastille <130799488+dalybastille@users.noreply.github.com>
2023-09-06 16:37:20 -0600

    Update algo.py

--- algo.py
+++ algo.py
@@ -7,6 +7,7 @@ import httpx
 import asyncio
 import json
 import ndjson
+import yaml
 
 # Configure logging
 log_file = "app.log"  # Specify the log file name here
@@ -24,22 +25,41 @@ logger.addHandler(file_handler)
 app = FastAPI()
 
 # Read configuration values from the JSON file
-with open("config.json", "r") as config_file:
-    config = json.load(config_file)
+#with open("config.json", "r") as config_file:
+#    config = json.load(config_file)
+
+# Read configuration values from the YAML file
+with open("config.yaml", "r") as config_file:
+    config = yaml.safe_load(config_file)
 
 # Extract configuration values
 source_host = config["source_host"]
 source_path = config["source_path"]
+source_port = config["source_port"]
 target_host = config["target_host"]
-target_path = config["target_path"]
-strobe_on_path = config["strobe_on_path"]
-strobe_off_path = config["strobe_off_path"]
 strobe_on_payload = config["strobe_on_payload"]
-default_target_payload = config["default_target_payload"]
-
-# Define Basic Authentication credentials
 auth_username = config["auth_username"]
 auth_password = config["auth_password"]
+clear_time = config["clear_time"]
+monitored_protocols = config["monitored_protocols"]
+allowed_tags = config["allowed_tags"]
+
+# Algo paths defined
+target_path = "/api/controls/screen/start"
+strobe_on_path = "/api/controls/strobe/start"
+strobe_off_path = "/api/controls/strobe/stop"
+
+#CLEAR JSON defined
+default_target_payload = {
+    "type": "image",
+    "text1": "CLEAR",
+    "textColor": "green",
+    "textFont": "roboto",
+    "textPosition": "middle",
+    "textScroll": "0",
+    "textScrollSpeed": "4",
+    "textSize": "medium"
+    }
 
 # Create Basic Authentication credentials
 auth = (auth_username, auth_password)
@@ -52,13 +72,15 @@ new_query_made = False
 
 reset_task = None
 
+# Resets new alert flag to false. Triggers screen CLEAR after configured seconds.
 async def reset_new_query_flag():
-    await asyncio.sleep(30)  # Sleep for X seconds
+    await asyncio.sleep(clear_time)  # Sleep for X seconds
     global new_query_made
     new_query_made = False
 
+# Resets screen to clear.
 async def send_default_payload():
-    await asyncio.sleep(30)  # Sleep for X seconds
+    await asyncio.sleep(clear_time)  # Sleep for X seconds
     global new_query_made
     logger.info("new_query_made value: %s", new_query_made) 
     if not new_query_made:
@@ -87,26 +109,18 @@ async def send_default_payload():
 
             # Check if the request was successful
             if response.status_code == 200:
-                logger.info("Default payload sent due to inactivity.")
+                logger.info("Clearing alerts due to not receiving new Zone Detections for %s seconds.", clear_time)
             else:
-                logger.error("Failed to send the default payload due to inactivity.")
+                logger.error("Failed to clear alerts due to error. Recommend checking Algo.")
 
+#Handles JSON payload.
 async def transfer_data(payload: dict, background_tasks: BackgroundTasks):
     # Log the incoming JSON payload for debugging
     logger.info("Received JSON payload: %s", payload)
     global new_query_made
     new_query_made = True  # Set the flag to True when a new source query is made
 
-    background_tasks.add_task(send_default_payload)
-
     global reset_task
-
-    # Cancel any previously scheduled reset_new_query_flag task
-    if reset_task and not reset_task.done():
-        reset_task.cancel()
-
-    # Schedule the reset_new_query_flag function to run in the background after 30 seconds
-    reset_task = asyncio.create_task(reset_new_query_flag())
 
     async with httpx.AsyncClient() as client:
         # Construct the complete source URL
@@ -147,52 +161,70 @@ async def transfer_data(payload: dict, background_tasks: BackgroundTasks):
         logger.info("zone_name details: %s", zone_name) 
 
         # Check if the "protocol" value is "wifi" in the source payload
-        if protocol in ["cellular", "wifi", "ble"] and "authorized" not in tags: 
-            # Create the target payload with "text1" set to "ALERT - [protocol] in [zone] - ALERT"
-            target_payload = {
-                "type": "image",
-                "text1": f"ALERT - {protocol} in {zone_name} - Vendor: {vendor} - ALERT",
-                "textColor": "orange",
-                "textFont": "roboto",
-                "textPosition": "middle",
-                "textScroll": "1",
-                "textScrollSpeed": "4",
-                "textSize": "medium"
-            }
+        if protocol in monitored_protocols and allowed_tags not in tags: 
+            if all(tag not in tags for tag in allowed_tags):
+                # Create the target payload with "text1" set to "ALERT - [protocol] in [zone] - ALERT"
+                target_payload = {
+                    "type": "image",
+                    "text1": f"ALERT - {protocol} in {zone_name} - Vendor: {vendor} - ALERT",
+                    "textColor": "orange",
+                    "textFont": "roboto",
+                    "textPosition": "middle",
+                    "textScroll": "1",
+                    "textScrollSpeed": "4",
+                    "textSize": "medium"
+                }
 
-            # Calculate the MD5 hash of the default target payload
-            payload_json = json.dumps(default_target_payload, separators=(',', ':'), sort_keys=True)
-            md5_hash = hashlib.md5(payload_json.encode()).digest()
-            md5_base64 = base64.b64encode(md5_hash).decode()
+                # Cancel any previously scheduled reset_new_query_flag task
+                if reset_task and not reset_task.done():
+                    reset_task.cancel()
 
-            # Add the Content-MD5 header to the request
-            headers["Content-MD5"] = md5_base64
+                # Schedule the reset_new_query_flag function to run in the background after 30 seconds
+                reset_task = asyncio.create_task(reset_new_query_flag())
+                
+                # Start a background job to CLEAR alerts
+                background_tasks.add_task(send_default_payload)
 
-            #Log
-            logger.info("Target JSON: %s", target_payload)
+                # Calculate the MD5 hash of the default target payload
+                payload_json = json.dumps(target_payload, separators=(',', ':'), sort_keys=True)
+                md5_hash = hashlib.md5(payload_json.encode()).digest()
+                md5_base64 = base64.b64encode(md5_hash).decode()
 
-            # Send the modified JSON payload to the target URL
-            response = await client.post(target_url, json=target_payload, auth=auth, headers=headers)
-            response = await client.post(target_strobe_on_url, json=strobe_on_payload, auth=auth, headers=headers)
+                # Add the Content-MD5 header to the request
+                headers["Content-MD5"] = md5_base64
 
-            # Log the headers of the target request
-            logger.info("Headers of the target request: %s", response.request.headers)
+                #Log
+                logger.info("Target JSON: %s", target_payload)
 
-            # Check if the request was successful
-            if response.status_code == 200:
-                logger.info("Data transferred successfully.")
-                return {"message": "Data transferred successfully."}
-            else:
-                logger.info("Response: %s", response)
-                error_text = response.text
-                logger.error("Failed to send data to the target URL. Error text: %s", error_text)
-                return {"error": "Failed to send data to the target URL."}
+                # Send the modified JSON payload to the target URL
+                response = await client.post(target_url, json=target_payload, auth=auth, headers=headers)
+                response = await client.post(target_strobe_on_url, json=strobe_on_payload, auth=auth, headers=headers)
+
+                # Log the headers of the target request
+                logger.info("Headers of the target request: %s", response.request.headers)
+
+                # Check if the request was successful
+                if response.status_code == 200:
+                    logger.info("Data transferred successfully.")
+                    return {"message": "Data transferred successfully."}
+                else:
+                    logger.info("Response: %s", response)
+                    error_text = response.text
+                    logger.error("Failed to send data to the target URL. Error text: %s", error_text)
+                    return {"error": "Failed to send data to the target URL."}
         
+            else:
+                # If "protocol" is not above, keep the target payload as is
+                target_payload = default_target_payload
+                logger.info("Not sending alert due to known tag.")
+
         else:
             # If "protocol" is not above, keep the target payload as is
             target_payload = default_target_payload
+            logger.info("Not sending alert due to unknown protocol.")
 
-@app.post("/zone-detections")
+# Listener. Converts NDJSON to JSON.
+@app.post(source_path)
 async def receive_ndjson(request: Request, background_tasks: BackgroundTasks):
     try:
         # Read the request body as bytes
@@ -218,4 +250,4 @@ async def receive_ndjson(request: Request, background_tasks: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=source_host, port=source_port)
