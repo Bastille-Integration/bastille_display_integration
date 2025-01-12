407d3a63b37e3b4b8a14e4c457a2b614ecc9e658
dalybastille <jdaly@bastille.io>
2025-01-12 13:27:59 -0700

    Added webhook response.

--- main.py
+++ main.py
@@ -127,7 +127,11 @@ async def receive_ndjson(request: Request, background_tasks: BackgroundTasks):
 
     # Create alert based on Bastille webhook
     webhook = await request.body()
-    create_alert(webhook)
+    try:
+        create_alert(webhook)
+        return "success"
+    except Exception as e:
+        return "failure"
 
     # Cancel any previously scheduled reset_new_query_flag task
     if reset_task and not reset_task.done():
